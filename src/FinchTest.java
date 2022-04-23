af234d03bb8e395c4df0ae1f2240476a1e3baa5b
Fostiriy <burakov.aa@students.dvfu.ru>
2022-04-23 20:59:03 +1000

    Initial commit

--- src/FinchTest.java
+++ src/FinchTest.java
@@ -2,13 +2,6 @@ public class FinchTest {
     public static void main(String[] args) {
         Finch myFinch = new Finch("A");
 
-//        for (int i = 0; i < 10; i++) {
-//            myFinch.setBeak(100, 100, 0);
-//            myFinch.pause(1);
-//            myFinch.setBeak(0, 0, 0);
-//            myFinch.pause(1);
-//        }
-
         int angle = 90;
         int speed = 100;
         int distance = 50;
