47e332a8c5bf7cc38765e86571a4ad48b8d14279
Fostiriy <burakov.aa@students.dvfu.ru>
2022-05-26 00:40:43 +1000

    Исправить проблемы, увиденные на практике. Робота нужно смазать

--- src/FinchController.java
+++ src/FinchController.java
@@ -11,26 +11,14 @@ public class FinchController {
     }
 
     public void startTest() { // Тестовый режим, Лёша писал в мейне
-        System.out.println("Go forward");
-
-        int temp = 50;
-        int speed1 = 100, speed2 = 50;
-        for (int i = 0; i < temp; i++) {
-            finch.setMotors(0, speed1);
-        }
         turnAround();
-        for (int i = 0; i < temp; i++) {
-            finch.setMotors(speed1, speed2);
-        }
-
-        goBack();
     }
 
     public void start() { // Рандомный режим
         int maxSpeed = 100; // Типа конфиг для значений рандомайзера
-        int minSpeed = -100;
-        int maxLength = 100;
-        int minLength = 10;
+        int minSpeed = 0;
+        int maxLength = 200;
+        int minLength = 50;
 
         System.out.println("Go recon");
         Random random = new Random();
@@ -46,10 +34,14 @@ public class FinchController {
                 if (isInterrupted()) {
                     isActive = false;
                     length = i + 1;
+                    System.out.printf("%15d%15d%15d%n", length, left, right);
+                    MoveCommand command = new MoveCommand(length, left, right);
+                    commands.add(command);
                     break;
                 }
             }
 
+            System.out.printf("%15d%15d%15d%n", length, left, right);
             MoveCommand command = new MoveCommand(length, left, right);
             commands.add(command);
         }
@@ -59,15 +51,19 @@ public class FinchController {
 
     private void goBack() {
         System.out.println("Go backward");
+        finch.pause(0.2);
         turnAround();
+        finch.pause(0.2);
 
         for (int i = commands.size() - 1; i >= 0; i--) {
             MoveCommand command = commands.get(i);
             int length = command.getLength();
-            int left = command.getLeftSpeed();
-            int right = command.getRightSpeed();
+            int left = command.getRightSpeed(); // Зеркально, чтобы пойти по тому же пути
+            int right = command.getLeftSpeed();
 
-            for (int j = 0; i < length; i++) {
+            System.out.printf("%15d%15d%15d%n", length, left, right);
+
+            for (int j = 0; j < length; j++) {
                 finch.setMotors(left, right);
             }
         }
@@ -75,7 +71,7 @@ public class FinchController {
 
     private void turnAround() {
         // поворот на 180 через правое плечо
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 55; i++) {
             finch.setMotors(100, -100);
         }
     }
@@ -83,6 +79,6 @@ public class FinchController {
     private boolean isInterrupted() {
         int lightLimit = 90;
 //		System.out.println(finch.getLight(Direction.LEFT) + " " + finch.getLight(Direction.RIGHT));
-        return finch.getLight(Direction.LEFT) <= lightLimit && finch.getLight(Direction.RIGHT) <= lightLimit;
+        return finch.getLight(Direction.LEFT) >= lightLimit || finch.getLight(Direction.RIGHT) >= lightLimit;
     }
 }
